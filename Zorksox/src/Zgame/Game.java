023ada6b9c66f7e61a98e971f79306f4c2c57672
Kevin <zorksox@gmail.com>
2014-01-22 00:18:50 -0600

    I added a comment

--- Zorksox/src/Zgame/Game.java
+++ Zorksox/src/Zgame/Game.java
@@ -15,6 +15,7 @@ import org.newdawn.slick.geom.Line;
 
 public class Game extends BasicGame 
 {
+	//adding a comment
 	static boolean boundingBoxes = false;
 	static boolean usingKeyboard = false;
 	static double lDeadZone = 0.0;
